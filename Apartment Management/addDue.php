e8a76f092e6029f7c6fafe2c929a253e9d41655a
Kursadcc <72594820+Kursadcc@users.noreply.github.com>
2021-02-11 18:47:07 +0300

    Delete Apartment Management directory

--- Apartment Management/addDue.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('db_connection.php');
-
-    $query = "UPDATE dues SET  debt=debt+45 ";
-    $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-    header('Location:dues.php');
-?>
